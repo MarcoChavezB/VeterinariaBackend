0bb6f19f086f699d21419def5796d923a36fdc9d
Marco <marco1102004@gmail.com>
2023-08-19 16:52:43 -0600

    funciones agregadas

--- public/index.php
+++ public/index.php
@@ -223,25 +223,19 @@ Router::post('/GenerarConsultasFecha',[GenerarConsultasController::class, 'Gener
 Router::post('/BuscarMedicamentos',[GenerarConsultasController::class, 'BuscarMedicamentos']);
 Router::post('/RegistroConsulta',[GenerarConsultasController::class, 'RegistroConsulta']);
 Router::post('/TServicios',[GenerarConsultasController::class, 'TServicios']);
-
 Router::get('/total_citas', [MostrarProductosController::class, 'cantidad_citas']);
 Router::get('/total_ventas', [MostrarProductosController::class, 'cantidad_ventas']);
 Router::get('/monto_total', [MostrarProductosController::class, 'montoTotal']);
-
 Router::post('/productoxcadena', [MostrarProductosController::class, 'productoporcadena']);
-
 Router::post('/productopublicoporcadena', [MostrarProductosController::class, 'productopublicoporcadena']);
-
-
 Router::get('/citas_total', [citasController::class, 'citasTot']);
 Router::get('/citas_aceptadas', [citasController::class, 'citasAceptadas']);
-
 Router::post('/citas_id', [citasController::class, 'cita_id']);
 Router::post('/citasResponse', [citasController::class, 'rechazar_aceptar_cita']);
-
 Router::post('/citasAceptadasResponse', [citasController::class, 'rechazar_completar_cita']);
 Router::get('/verificacion', [LoginController::class, 'verificacion']);
-
 Router::post('/venta', [VentasController::class, 'venta']);
-
+Router::post('/CancelarCompra', [VentasController::class, 'revertir']);
 Router::get('/GenerarTiket', [VentasController::class, 'tiket']);
+Router::post('/mandarCorreo', [citasController::class, 'correo_cita']);
+Router::post('/revisar_producto', [ProductoController::class, 'verificar_producto']);
