55d1b9f659b06665c34bfa7299264fc60cf34c42
anthony <tony20402014@gmail.com>
2023-08-21 17:40:03 -0600

    cambios

--- public/index.php
+++ public/index.php
@@ -61,7 +61,7 @@ Router::post('/ReporteConsultasFecha',[ReportesController::class, 'ReporteConsul
 Router::post('/ReporteConsultasCliente',[ReportesController::class, 'ReporteConsultasCliente']);
 
 Router::post('/ReporteGralCitasRechazadas',[ReportesController::class, 'ReporteGralCitasRechazadas']);
-Router::post('/ReporteCitasRechazadasCliente',[ReportesController::class, 'ReporteCitasRechazadasCliente']);
+Router::post('/ReporteCiasRechazadasCliente',[ReportesController::class, 'ReporteCitasRechazadasCliente']);
 Router::post('/ReporteCitasRechazadasFecha',[ReportesController::class, 'ReporteCitasRechazadasFecha']);
 Router::post('/ReporteGeneralOrdenesCompra',[ReportesController::class, 'ReporteGeneralOrdenesCompra']);
 Router::post('/ReporteGeneralOrdenesCompraPagadas',[ReportesController::class, 'ReporteGeneralOrdenesCompraPagadas']);
